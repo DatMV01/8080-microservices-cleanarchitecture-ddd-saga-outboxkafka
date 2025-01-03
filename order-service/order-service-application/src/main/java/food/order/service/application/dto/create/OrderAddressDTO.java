dfdad36c590c5de5e744d8112d97bf6c06a041ee
DatMV <datmv01@gmail.com>
2025-01-03 17:39:37 +0700

    add: Add OrderDataMapper

--- order-service/order-service-application/src/main/java/food/order/service/application/dto/create/OrderAddressDTO.java
+++ order-service/order-service-application/src/main/java/food/order/service/application/dto/create/OrderAddressDTO.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.NotNull;
 @Getter
 @Builder
 @AllArgsConstructor
-public class OrderAddress {
+public class OrderAddressDTO {
     @NotNull
     @Max(value = 50)
     private final String street;

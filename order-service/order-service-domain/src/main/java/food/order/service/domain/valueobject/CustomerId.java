580aa68e6ac5c437f9061c539fc27b2f8f54b17e
DatMV <datmv01@gmail.com>
2025-01-03 15:39:22 +0700

    add: Add value object class

---
+++ order-service/order-service-domain/src/main/java/food/order/service/domain/valueobject/CustomerId.java
@@ -0,0 +1,9 @@
+package food.order.service.domain.valueobject;
+
+import java.util.UUID;
+
+public class CustomerId extends BaseId<UUID> {
+    public CustomerId(UUID value) {
+        super(value);
+    }
+}

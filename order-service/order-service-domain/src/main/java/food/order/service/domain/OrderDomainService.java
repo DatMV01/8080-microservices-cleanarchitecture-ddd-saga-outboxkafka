8da998e8d13fed71ceec3fb1df9cabb393b3e5c1
DatMV <datmv01@gmail.com>
2025-01-03 17:14:21 +0700

    add: Add OrderDomainService

---
+++ order-service/order-service-domain/src/main/java/food/order/service/domain/OrderDomainService.java
@@ -0,0 +1,23 @@
+package food.order.service.domain;
+
+import food.order.service.domain.aggregate.OrderAggregate;
+import food.order.service.domain.aggregate.RestaurantAggregate;
+import food.order.service.domain.event.OrderCancelledEvent;
+import food.order.service.domain.event.OrderCreatedEvent;
+import food.order.service.domain.event.OrderPaidEvent;
+import food.order.service.domain.event.publisher.DomainEventPublisher;
+
+import java.util.List;
+
+public interface OrderDomainService {
+
+    OrderCreatedEvent validateAndInitiateOrder(OrderAggregate order, RestaurantAggregate restaurant, DomainEventPublisher<OrderCreatedEvent> orderCreatedEventDomainEventPublisher);
+
+    OrderPaidEvent payOrder(OrderAggregate order, DomainEventPublisher<OrderPaidEvent> orderPaidEventDomainEventPublisher);
+
+    void approveOrder(OrderAggregate order);
+
+    OrderCancelledEvent cancelOrderPayment(OrderAggregate order, List<String> failureMessages, DomainEventPublisher<OrderCancelledEvent> orderCancelledEventDomainEventPublisher);
+
+    void cancelOrder(OrderAggregate order, List<String> failureMessages);
+}

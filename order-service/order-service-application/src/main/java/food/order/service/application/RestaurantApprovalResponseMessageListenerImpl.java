c1b580b46710fae9b00c66fcbf4b0f35d5a01a88
DatMV <datmv01@gmail.com>
2025-01-03 19:17:51 +0700

    add: Add Ports Handlers

---
+++ order-service/order-service-application/src/main/java/food/order/service/application/RestaurantApprovalResponseMessageListenerImpl.java
@@ -0,0 +1,22 @@
+package food.order.service.application;
+
+import food.order.service.application.dto.message.RestaurantApprovalResponse;
+import food.order.service.application.ports.input.message.restaurant.RestaurantApprovalResponseMessageListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Slf4j
+@Validated
+@Service
+public class RestaurantApprovalResponseMessageListenerImpl implements RestaurantApprovalResponseMessageListener {
+    @Override
+    public void orderApproved(RestaurantApprovalResponse restaurantApprovalResponse) {
+
+    }
+
+    @Override
+    public void orderRejected(RestaurantApprovalResponse restaurantApprovalResponse) {
+
+    }
+}

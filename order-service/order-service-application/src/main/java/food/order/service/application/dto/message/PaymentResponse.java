e64d5177c5732f9af3f4e66987c6081ccd995bac
DatMV <datmv01@gmail.com>
2025-01-04 14:54:51 +0700

    add: Add CreateOrderKafkaMessagePublisher

--- order-service/order-service-application/src/main/java/food/order/service/application/dto/message/PaymentResponse.java
+++ order-service/order-service-application/src/main/java/food/order/service/application/dto/message/PaymentResponse.java
@@ -1,11 +1,17 @@
 package food.order.service.application.dto.message;
 
 import food.order.service.domain.valueobject.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.List;
 
+@Getter
+@Builder
+@AllArgsConstructor
 public class PaymentResponse {
     private String id;
     private String sagaId;

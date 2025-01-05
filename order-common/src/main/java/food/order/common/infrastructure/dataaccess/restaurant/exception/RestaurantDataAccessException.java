3fc2eb9faa133de1625ca3df7a093e04bbd62549
DatMV <datmv01@gmail.com>
2025-01-05 20:42:41 +0700

    add: Add Restaurant entity in order-common module

---
+++ order-common/src/main/java/food/order/common/infrastructure/dataaccess/restaurant/exception/RestaurantDataAccessException.java
@@ -0,0 +1,8 @@
+package food.order.common.infrastructure.dataaccess.restaurant.exception;
+
+public class RestaurantDataAccessException extends RuntimeException {
+
+    public RestaurantDataAccessException(String message) {
+        super(message);
+    }
+}

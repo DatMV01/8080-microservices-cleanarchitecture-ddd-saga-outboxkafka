74efc1ce344aa2f3693476e1ff2810c7c437824a
DatMV <datmv01@gmail.com>
2025-01-05 22:00:42 +0700

    add: Complete order-restaurant-application module

---
+++ order-restaurant/order-restaurant-application/src/main/java/food/order/restaurant/application/exception/RestaurantApplicationServiceException.java
@@ -0,0 +1,13 @@
+package food.order.restaurant.application.exception;
+
+import food.order.common.domain.exception.DomainException;
+
+public class RestaurantApplicationServiceException extends DomainException {
+    public RestaurantApplicationServiceException(String message) {
+        super(message);
+    }
+
+    public RestaurantApplicationServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
